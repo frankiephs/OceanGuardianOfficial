ed46c48ae9d4705bf36257bce96bf9579ccc163c
Chien <cn20251@student.phs.school.nz>
2023-08-31 09:34:02 +1200

    added players and saved additional variables

--- Main.py
+++ Main.py
@@ -17,24 +17,31 @@ pygame.display.set_caption("My Pygame")
 
 
 
-class game():
+class Game():
     def __init__(self):
 
-
-        
-
+        """
         # bg menu
         self.bg_menu_image
 
         # bg game
         self.bg_game_image
+        """
+
+
 
         # character
-        self.character_image
-        self.character_x
-        self.character_y
-        self.character_rect
+        self.character_image = ""
+        self.character_color = (255,0,0)
+        self.character_x = SCREEN_WIDTH // 2
+        self.character_y = SCREEN_HEIGHT // 2
+        self.character_width = 50
+        self.character_height = 50
+        self.character_rect = pygame.Rect(self.character_x, self.character_y, self.character_width, self.character_height)
 
+        
+    # archive
+    """
         # button
         self.button_color
         self.button_image
@@ -56,11 +63,16 @@ class game():
 
         # high score
         self.scores = []
-
-
+    """
+    def draw(self):
+        pygame.draw.rect(screen, self.character_color, self.character_rect)
+        pygame.display.flip()
 
 
         
+
+
+game = Game()
 
 
 
@@ -78,9 +90,8 @@ while running:
     # Clear the screen
     screen.fill(BACKGROUND_COLOR)
 
-    # Update game logic here
 
-    # Draw game elements here
+    game.draw()
 
     # Update the display
     pygame.display.flip()
